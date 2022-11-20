fa6484fefc8bdddc903c728fc140697616ccef8a
vamsi434 <vamsibusa@gmail.com>
2022-11-20 10:32:50 +0530

    tga

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
     VPC_CIDR = var.VPC_CIDR
     //PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
     //PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
-    //ENV = var.ENV
+    = var.ENV
 }
 //variable "PUBLIC_SUBNET_CIDR" {}
 //variable "PRIVATE_SUBNET_CIDR" {}
