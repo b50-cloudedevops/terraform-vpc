9c4182900960d80e1c40a95f249725e681a64b24
vamsi434 <vamsibusa@gmail.com>
2022-11-20 11:17:06 +0530

    tk

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 module "vpc" {
     source = "./vendor/modules/vpc"
     VPC_CIDR = var.VPC_CIDR
-    //PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-    //PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+    PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
     ENV = var.ENV
+    AZ = var.AZ
 }
 //variable "PUBLIC_SUBNET_CIDR" {}
 //variable "PRIVATE_SUBNET_CIDR" {}
